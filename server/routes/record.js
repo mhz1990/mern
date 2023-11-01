144b5ca626f671e283ede75cec0cdd5fefe8a88b
Matt Hoadley <matt.hoadley1990@gmail.com>
2023-11-01 18:35:06 +0800

    creates function to create record

--- server/routes/record.js
+++ server/routes/record.js
@@ -36,6 +36,20 @@ recordRoutes.route("/record/:id").get(function (req, res) {
         });
 });
 
+// This section will help you create a new record.
+recordRoutes.route("/record/add").post(function (req, response) {
+    let db_connect = dbo.getDb();
+    let myobj = {
+        name: req.body.name,
+        position: req.body.position,
+        level: req.body.level,
+    };
+    db_connect.collection("records").insertOne(myobj, function (err, res) {
+        if (err) throw err;
+        response.json(res);
+    });
+});
+
 // This section will help you update a record by id.
 recordRoutes.route("/update/:id").post(function (req, response) {
     let db_connect = dbo.getDb();
