072737d52f68cc77b9eb5d355f67465053c2617b
Matt Hoadley <matt.hoadley1990@gmail.com>
2023-11-01 16:11:37 +0800

    partially writes record.js

---
+++ server/routes/record.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+// recordRoutes is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /record.
+const recordRoutes = express.Router();
+
+// This will help us connect to the database
+const dbo = require("mongodb").ObjectId;
